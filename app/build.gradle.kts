4e0524134caf089615866e3c58a92fec85653b5f
Maxime Boulenger <maxime.boulenger@etud.u-picardie.fr>
2026-05-12 12:37:54 +0200

    Setup Jetpack Compose + composants AppButton et CreateEvent pour tester.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     debugImplementation(libs.androidx.compose.ui.tooling)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.icons.extended)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
--- app/src/main/java/fr/miage/geoevent/ui/events/pages/CreateEvent.kt
+++ app/src/main/java/fr/miage/geoevent/ui/events/pages/CreateEvent.kt
@@ -7,13 +7,21 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import fr.miage.geoevent.ui.events.components.AppButton
+import fr.miage.geoevent.ui.events.components.AppTextField
 
 @Composable
 fun CreateEvent() {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -43,6 +51,23 @@ fun CreateEvent() {
                 println("Bouton cliqué")
             },
             enabled = false
+        )
+
+        AppTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = "Adresse email",
+            placeholder = "exemple@gmail.com",
+            required = true
+        )
+
+        AppTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = "Mot de passe",
+            placeholder = "Votre mot de passe",
+            required = true,
+            isPassword = true
         )
     }
 }
