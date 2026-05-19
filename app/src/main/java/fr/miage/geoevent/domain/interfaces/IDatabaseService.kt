cd53b9cb422d8c7029bee4805c89ae05a705ffdd
NoahFsc <noah.fsca@gmail.com>
2026-05-19 17:23:18 +0200

    Correction build
    # Conflicts:
    #	app/src/main/java/fr/miage/geoevent/domain/interfaces/IDatabaseService.kt

--- app/src/main/java/fr/miage/geoevent/domain/interfaces/IDatabaseService.kt
+++ app/src/main/java/fr/miage/geoevent/domain/interfaces/IDatabaseService.kt
@@ -8,10 +8,7 @@ interface IDatabaseService {
     suspend fun addEvent(event: GeoEvent)
     suspend fun getAllEvents(): List<GeoEvent>
     fun listenToEventsRealtime(): Flow<List<GeoEvent>>
-    // Télécharge une image dans le bucket et retourne son URL publique
     suspend fun uploadImage(fileName: String, bytes: ByteArray): String
-
     suspend fun createProfile(user: User)
-
     suspend fun deleteProfile(userId: String)
 }
